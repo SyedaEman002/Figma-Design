306c13123ac7ac929269f7d6b3c5ab35dfbcc645
SyedaEman002/SyedaEman002 <emansyeda189@gmail.com>
2025-02-20 11:30:37 +0500

    page,header

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -9,7 +9,7 @@ import Header from "../components/header";
         <div className="w-1/2 flex flex-col justify-center items-start m-12">
         <h1 className="text-[40px] font-bold font-[libre-bodoni]">IMPECCABLE</h1>
         <h1 className="text-[40px] font-bold font-[libre-bodoni]">CRAFTMANSHIP AND</h1>
-        <h1 className="text-[40px] font-bold font-[libre-bodoni]">FINESSE</h1>
+        <h1 className="text-[40px] font-bold font-[libre-bodoni]">FINESSE..</h1>
         <div>
         <p className="text-[22px] w-[571px] font-[libre-bodoni] background: rgba(120, 112, 84, 1); "> An example of intricate workmanship and detail of elegant necklaces,lond and short chains form a part of our desirable collection.</p></div>
         <div className="p-2 rounded-md background: rgba(162, 152, 117, 1);">
