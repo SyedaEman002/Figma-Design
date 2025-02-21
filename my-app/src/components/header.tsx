964861aefec2091a5631d62ad768f2f8b00637b6
SyedaEman002/SyedaEman002 <emansyeda189@gmail.com>
2025-02-21 15:55:09 +0500

    page,header

--- my-app/src/components/header.tsx
+++ my-app/src/components/header.tsx
@@ -3,7 +3,7 @@ function Header(){
     return(
         <div className=" bg-[rgba(162,152,117,1)] width-[43px];">
         <div>
-            <h1 className="p-[20px] top-[20px] font-[Rye] text-slate-50 font-bold text-[28px]">MANZZARI.</h1>
+            <h1 className="p-[20px] top-[20px] font-[Rye] text-slate-50 font-bold text-[28px]">MANZARI.</h1>
         </div>
         </div>
     )  
